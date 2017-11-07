1e321cca3fff549a75947dca82b9761515f2a7e8
Argir Argirov <dj.giro@gmail.com>
2017-11-07 13:00:01 -0500

    api call made

---
+++ nov7_api.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+
+toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries/toronto-wards/')
+
+toronto_wards_json = JSON.parse(toronto_wards_response.body)
+
+toronto_wards_json['objects'].each do |ward|
+  p ward['name']
+
+end
+p '____________________________'
+house_commons_response = HTTParty.get('https://represent.opennorth.ca/representatives/house-of-commons')
+
+house_commons_json = JSON.parse(house_commons_response.body)
+
+persons = []
+house_commons_json['objects'].sort.each do |person|
+  p person['name']
+
+end
